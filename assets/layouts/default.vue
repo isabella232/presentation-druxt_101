5ddd23bb72b7d415d382354d9b36216e67a7eb55
Stuart Clark <stuart@realityloop.com>
2021-07-14 16:40:39 +1000

    Updated demo assets.

---
+++ assets/layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <DruxtSite theme="bartik">
+    <template #default="{ props }">
+      <b-container>
+        <DruxtBlockRegion v-bind="props.header" />
+
+        <DruxtBlockRegion v-bind="props.content" />
+      </b-container>
+    </template>
+  </DruxtSite>
+</template>
